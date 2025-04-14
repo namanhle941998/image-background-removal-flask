caae1d986593926c846a65de857ca94cdc95677c
caots <honghoang1232@gmail.com>
2025-04-14 15:25:38 +0700

    finish

--- app.py
+++ app.py
@@ -1,43 +1,27 @@
-from flask import Flask, render_template, request, send_file  
-from rembg import remove  
-from PIL import Image  
-import os
+from flask import Flask, render_template, request, send_file
+from rembg import remove
+from PIL import Image
+from io import BytesIO
 
 app = Flask(__name__)
 
-# Configure upload folder  
-UPLOAD_FOLDER = 'static/uploads'  
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-# Configure Flask-Dropzone  
-app.config['DROPZONE_ALLOWED_FILE_CUSTOM'] = True  
-app.config['DROPZONE_ALLOWED_FILE_TYPE'] = 'image/*'  
-app.config['DROPZONE_MAX_FILE_SIZE'] = 3  
-app.config['DROPZONE_MAX_FILES'] = 1
-
-@app.route('/', methods=['GET', 'POST'])  
-def index():  
-    if request.method == 'POST':  
-        if 'file' not in request.files:  
-            return 'No file uploaded'  
-          
-        file = request.files['file']  
-        if file.filename == '':  
-            return 'No file selected'
-
-        # Save original image  
-        input_path = os.path.join(app.config['UPLOAD_FOLDER'], 'input.png')  
-        output_path = os.path.join(app.config['UPLOAD_FOLDER'], 'output.png')  
-        file.save(input_path)
-
-        # Remove background  
-        input_image = Image.open(input_path)  
-        output_image = remove(input_image)  
-        output_image.save(output_path)
-
-        return send_file(output_path, as_attachment=True)
-
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return 'No file uploaded', 400
+        file = request.files['file']
+        if file.filename == '':
+            return 'No file selected', 400
+        if file:
+            input_image = Image.open(file.stream)
+            output_image = remove(input_image, post_process_mask=True)
+            img_io = BytesIO()
+            output_image.save(img_io, 'PNG')
+            img_io.seek(0)
+            # return send_file(img_io, mimetype='image/png')  # Change download in separatre browser tab
+            return send_file(img_io, mimetype='image/png', as_attachment=True, download_name='_rmbg.png')
     return render_template('index.html')
 
-if __name__ == '__main__':  
-    app.run(debug=True)  
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True, port=5100)
